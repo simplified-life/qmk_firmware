7172ee94d82b2497601bbd1beedcfdf7c3bcd04b
Aleblazer <spenceizzle@gmail.com>
2023-07-30 21:54:52 -0500

    Updated encoder fix

--- keyboards/aleblazer/zodimacro/keymaps/default/keymap.c
+++ keyboards/aleblazer/zodimacro/keymaps/default/keymap.c
@@ -33,12 +33,6 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code(KC_VOLD);
         }
-    } else if (index == 1) {
-        if (clockwise) {
-              tap_code(KC_PGUP);
-        } else {
-           tap_code(KC_PGDN);
-        }
     }
     return true;
 }
