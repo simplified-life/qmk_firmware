c202d26a170e3a1d25478f38f23a1c6b0091b68f
simplified-life <55418461+simplified-life@users.noreply.github.com>
2024-09-11 17:49:24 +1000

    Left Shift as Enter (for mousing)

--- keyboards/aleblazer/zodiarkpi/keymaps/dualcirque_rb/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/dualcirque_rb/keymap.c
@@ -114,11 +114,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //QWERTY / BASE
 	[0] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS, KC_MUTE,  RGB_TOG, KC_EQL,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV, KC_BSLS,   KC_EQL, TT(1),     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL, KC_LCTL, KC_LBRC, KC_RBRC, KC_LGUI, KC_SPC,  LGUI(LSFT(KC_S)),  LCTL(LALT(KC_TAB)),KC_ENT,  KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS, KC_MUTE,  RGB_TOG, KC_EQL,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_SFTENT,KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV, KC_BSLS,   KC_EQL, TT(1),     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+      KC_LCTL,  KC_LCTL, KC_LBRC, KC_RBRC, KC_LGUI, KC_SPC,  LGUI(LSFT(KC_S)),  LCTL(LALT(KC_TAB)),KC_ENT,  KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 //NUMPAD
 	[1] = LAYOUT(
