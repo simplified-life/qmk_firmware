0fb7e93b442254d9eeee4cceb5215e32d238d95c
simplified-life <55418461+simplified-life@users.noreply.github.com>
2024-09-12 12:43:57 +1000

    Keymap Updates

--- keyboards/aleblazer/zodiarkpi/keymaps/dualcirque_rb/keymap.c
+++ keyboards/aleblazer/zodiarkpi/keymaps/dualcirque_rb/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TD(TD_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BSPC),
       KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_MINS, KC_MUTE,  RGB_TOG, KC_EQL,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       SC_SENT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,LT(1,KC_GRV), KC_BSLS,  KC_EQL,  TT(1),    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-      KC_LCTL, KC_LALT, KC_LBRC, KC_RBRC, KC_LGUI,  KC_SPC,            LSG(KC_S),  KC_SPC,          KC_ENT,  KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_LCTL, KC_LALT, KC_LBRC, KC_RBRC, KC_LGUI,  KC_SPC,            LSG(KC_S),  KC_SPC,          KC_ENT,  KC_DEL,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 //NUMPAD
 	[1] = LAYOUT(
